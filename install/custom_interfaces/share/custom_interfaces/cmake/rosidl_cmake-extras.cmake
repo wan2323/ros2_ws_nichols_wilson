7c577cac735517ae7d2bab597fa3f48b583cd830
wilson nichols <wilsonanichols@gmail.com>
2025-11-07 16:49:33 -0500

    cleaned build/install/log & updated gitignore

--- install/custom_interfaces/share/custom_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(custom_interfaces_IDL_FILES "srv/ResetPosition.idl")
-set(custom_interfaces_INTERFACE_FILES "srv/ResetPosition.srv;srv/ResetPosition_Request.msg;srv/ResetPosition_Response.msg")
